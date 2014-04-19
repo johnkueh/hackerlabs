c8b405c7052d5f5a5355abe6ee1f1c2008f21727
John Kueh <yoongfook@gmail.com>
2014-04-20 08:40:05 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackerhouse::Application.config.secret_key_base = 'cb9031224ad002ae5804b09b300bd08469d25b1243858e3159847f1b72d029a92aee9062ccec2a3f10c39e6b76e8bf9731aae04ec9481becc8d1429d84f66c98'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hackerhouse::Application.config.session_store :cookie_store, key: '_hackerhouse_session'
